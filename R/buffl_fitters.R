37cb02a57ec12e53e4e82c35cd3e5c1eff7988f4
Michael Stevens <michael.andre.stevens@gmail.com>
2019-08-27 16:20:12 +0200

    added model fitters

---
+++ R/buffl_fitters.R
@@ -0,0 +1,238 @@
+#' Process the arguments of a fitter function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return a dataframe
+#' @keywords internal
+make_data <- function(y, x=NULL) {
+  if (is.null(x)) {
+    data.frame(y=y)
+  } else {
+    data.frame(y=y, x=x)
+  }
+}
+
+#' Process the arguments of a fitter function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return a dataframe
+#' @keywords internal
+make_long_data <- function(y, x=NULL) {
+  if (is.null(x)) {
+    y %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(id = 1:dplyr::n()) %>%
+      tidyr::gather('alternative', 'y', -'id') %>%
+      dplyr::arrange(.data$id)
+  } else {
+      data.frame(y, x) %>%
+        dplyr::ungroup() %>%
+        dplyr::mutate(id = 1:dplyr::n()) %>%
+        tidyr::gather('alternative', 'y', -'id', -'x') %>%
+        dplyr::mutate(alternative=factor(.data$alternative)) %>%
+        dplyr::arrange(.data$id)
+  }
+}
+
+#' Process the arguments of a fitter function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return a formula
+#' @keywords internal
+make_formula <- function(y, x=NULL) {
+  if (is.null(x)) {
+    stats::as.formula("y ~ 1")
+  } else {
+    stats::as.formula("y ~ x")
+  }
+}
+
+
+#' Process the arguments of a fitter function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return a formula
+#' @keywords internal
+make_lmer_formula <- function(y, x=NULL) {
+  if (is.null(x)) {
+    formula <- stats::as.formula("y ~ alternative + (1|id)")
+  } else {
+    formula <- stats::as.formula("y ~ alternative * x + (1|id)")
+  }
+}
+
+
+#' Fit a linear model
+#'
+#' This model is used for slider questions
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_linear_model <- function(y, x=NULL) {
+
+  dfx <- make_data(y, x)
+  form <- make_formula(y, x)
+  fit <- stats::lm(form, data=dfx)
+  fit
+}
+
+
+#' Fit a linear model with a logistic link function
+#'
+#' This model is used for multiple choice questions with two alternatives
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_logistic_model <- function(y, x=NULL) {
+
+  dfx <- make_data(y, x)
+  dfx$y <- as.logical(dfx$y)
+  form <- make_formula(y, x)
+  fit <- stats::glm(form, data=dfx, family=stats::binomial)
+  fit
+}
+
+
+#' Fit a multinomial model
+#'
+#' This model is used for multiple choide questions with more than two unordered alternatives
+#'
+#' The default function uses the nnet::multinom function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_multinomial_model <- function(y, x=NULL) {
+
+  dfx <- make_data(y, x)
+  form <- make_formula(y, x)
+  fit <- nnet::multinom(form, data=dfx, trace=FALSE)
+  fit
+}
+
+
+#' Fit a multinomial model
+#'
+#' This model is used for multiple choide questions with more than two unordered alternatives
+#'
+#' Alternative implementation that uses the mlogit::mlogic function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_multinomial_model2 <- function(y, x=NULL) {
+
+  dfx <- make_data(y, x)
+
+  if (is.null(x)) {
+    dfxm <- mlogit::mlogit.data(dfx, choice="y", shape = "wide", alt.levels=levels(y))
+    fit <- mlogit::mlogit(y ~ 0 | 1, data = dfxm)
+  } else {
+    dfxm <- mlogit::mlogit.data(dfx, choice="y", shape = "wide", alt.levels=levels(y))
+    fit <- mlogit::mlogit(y ~ 0 | x, data = dfxm)
+  }
+  fit
+}
+
+
+#' Fit an ordinal model
+#'
+#' This model is used for multiple choide questions with more than two ordered alternatives
+#'
+#' The default function uses the MASS::polr function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_ordinal_model <- function(y, x=NULL) {
+
+  dfx <- make_data(y, x)
+  form <- make_formula(y, x)
+  fit <- MASS::polr(form, data=dfx, Hess=TRUE)
+  fit
+}
+
+
+#' Fit an ordinal model
+#'
+#' This model is used for multiple choide questions with more than two ordered alternatives
+#'
+#' Alternative implementation that uses the ordinal::clm function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_ordinal_model2 <- function(y, x=NULL) {
+
+  dfx <- make_data(y, x)
+  form <- make_formula(y, x)
+  fit <- ordinal::clm(form, data=dfx)
+  fit
+}
+
+
+#' Fit a mixed linear model with a logistic link function
+#'
+#' This model is used for checkbox questions (multiple choice where more than one alternative can be selected)
+#'
+#' The default function uses the lme4:glmer function
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_mixed_model <- function(y, x=NULL) {
+
+  dfx <- make_long_data(y, x)
+  form <- make_lmer_formula(y, x)
+  fit <- suppressWarnings(lme4::glmer(form, data=dfx, family=stats::binomial))
+  fit
+}
+
+
+
+#' Fit a mixed linear model with a logistic link function
+#'
+#' This model is used for checkbox questions (multiple choice where more than one alternative can be selected)
+#'
+#' Alternative implementation that uses the stats::glm function (and fits a fixed effects model instead of a mixed effects model)
+#'
+#' @param y the dependent variable
+#' @param x an optional predictor
+#'
+#' @return the model fit
+#' @export
+fit_mixed_model2 <- function(y, x=NULL) {
+
+  dfx <- make_long_data(y, x)
+  form <- make_formula(y, x)
+
+  fit <- stats::glm(form, data=dfx, family=stats::binomial)
+  fit
+}
+
+
+
+
