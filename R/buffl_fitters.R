7cf4d9f74b5eb8076d35b9a816e807cef14a46c1
Michael Stevens <michael.andre.stevens@gmail.com>
2019-09-23 16:41:29 +0200

    rewrite of input to the fitter functions - now they start on a tibble with one (the DV) or two (an optional covariate) variables. checkbox variables are converted just before the fit

--- R/buffl_fitters.R
+++ R/buffl_fitters.R
@@ -1,126 +1,51 @@
-#' Process the arguments of a fitter function
-#'
-#' @param y the dependent variable
-#' @param x an optional predictor
-#'
-#' @return a dataframe
-#' @keywords internal
-make_data <- function(y, x=NULL) {
-  if (is.null(x)) {
-    data.frame(y=dplyr::pull(y))
-  } else {
-    data.frame(y=dplyr::pull(y), x=dplyr::pull(x))
-  }
-}
-
-
-#' Process the arguments of a fitter function
-#'
-#' @param y the dependent variable
-#' @param x an optional predictor
-#'
-#' @return a dataframe
-#' @keywords internal
-make_long_data <- function(y, x=NULL) {
-  if (is.null(x)) {
-    y %>%
-      dplyr::ungroup() %>%
-      dplyr::mutate(id = 1:dplyr::n()) %>%
-      tidyr::gather('alternative', 'y', -'id') %>%
-      dplyr::arrange(.data$id)
-  } else {
-      data.frame(y, x) %>%
-        dplyr::ungroup() %>%
-        dplyr::mutate(id = 1:dplyr::n()) %>%
-        tidyr::gather('alternative', 'y', -'id', -'x') %>%
-        dplyr::mutate(alternative=factor(.data$alternative)) %>%
-        dplyr::arrange(.data$id)
-  }
-}
-
-#' Process the arguments of a fitter function
-#'
-#' @param y the dependent variable
-#' @param x an optional predictor
-#'
-#' @return a formula
-#' @keywords internal
-make_formula <- function(y, x=NULL) {
-  if (is.null(x)) {
-    stats::as.formula("y ~ 1")
-  } else {
-    stats::as.formula("y ~ x")
-  }
-}
-
-
-#' Process the arguments of a fitter function
-#'
-#' @param y the dependent variable
-#' @param x an optional predictor
-#'
-#' @return a formula
-#' @keywords internal
-make_mixed_formula <- function(y, x=NULL) {
-  if (is.null(x)) {
-    formula <- stats::as.formula("y ~ alternative + (1|id)")
-  } else {
-    formula <- stats::as.formula("y ~ alternative * x + (1|id)")
-  }
-}
-
-
-#' Process the arguments of a fitter function
-#'
-#' @param y the dependent variable
-#' @param x an optional predictor
-#'
-#' @return a formula
-#' @keywords internal
-make_fixed_formula <- function(y, x=NULL) {
-  if (is.null(x)) {
-    formula <- stats::as.formula("y ~ alternative")
-  } else {
-    formula <- stats::as.formula("y ~ alternative * x")
-  }
-}
-
-
 #' Fit a linear model
 #'
 #' This model is used for slider questions
 #'
-#' @param y the dependent variable
-#' @param x an optional predictor
+#' @param data a dataframe with the dependent variable and an optional covariate
 #'
 #' @return the model fit
 #' @export
-fit_linear_model <- function(y, x=NULL) {
+fit_linear_model <- function(data) {
 
-  dfx <- make_data(y, x)
-  form <- make_formula(y, x)
-  fit <- stats::lm(form, data=dfx)
-  fit
+  if (ncol(data) == 1) {
+    names(data) <- "y"
+    form <- stats::as.formula("y ~ 1")
+  } else if(ncol(data) == 2) {
+    names(data) <- c("y", "x")
+    form <- stats::as.formula("y ~ x")
+  } else {
+    stop("fit_model expects two variables as input")
+  }
+
+  stats::lm(form, data=data)
 }
+
 
 
 #' Fit a linear model with a logistic link function
 #'
 #' This model is used for multiple choice questions with two alternatives
 #'
-#' @param y the dependent variable
-#' @param x an optional predictor
+#' @param data a dataframe with the dependent variable and an optional covariate
 #'
 #' @return the model fit
 #' @export
-fit_logistic_model <- function(y, x=NULL) {
+fit_logistic_model <- function(data) {
 
-  dfx <- make_data(y, x)
-  dfx$y <- as.logical(dfx$y)
-  form <- make_formula(y, x)
-  fit <- stats::glm(form, data=dfx, family=stats::binomial)
-  fit
+  if (ncol(data) == 1) {
+    names(data) <- "y"
+    form <- stats::as.formula("y ~ 1")
+  } else if(ncol(data) == 2) {
+    names(data) <- c("y", "x")
+    form <- stats::as.formula("y ~ x")
+  } else {
+    stop("fit_model expects two variables as input")
+  }
+
+  stats::glm(form, data=data, family=stats::binomial)
 }
+
 
 
 #' Fit a multinomial model
@@ -129,43 +54,23 @@ fit_logistic_model <- function(y, x=NULL) {
 #'
 #' The default function uses the nnet::multinom function
 #'
-#' @param y the dependent variable
-#' @param x an optional predictor
+#' @param data a dataframe with the dependent variable and an optional covariate
 #'
 #' @return the model fit
 #' @export
-fit_multinomial_model <- function(y, x=NULL) {
+fit_multinomial_model <- function(data) {
 
-  dfx <- make_data(y, x)
-  form <- make_formula(y, x)
-  fit <- nnet::multinom(form, data=dfx, trace=FALSE, model=TRUE)
-  fit
-}
-
-
-#' Fit a multinomial model
-#'
-#' This model is used for multiple choice questions with more than two unordered alternatives
-#'
-#' Alternative implementation that uses the mlogit::mlogic function
-#'
-#' @param y the dependent variable
-#' @param x an optional predictor
-#'
-#' @return the model fit
-#' @export
-fit_multinomial_model2 <- function(y, x=NULL) {
-
-  dfx <- make_data(y, x)
-
-  if (is.null(x)) {
-    dfxm <- mlogit::mlogit.data(dfx, choice="y", shape = "wide", alt.levels=levels(y))
-    fit <- mlogit::mlogit(y ~ 0 | 1, data = dfxm)
+  if (ncol(data) == 1) {
+    names(data) <- "y"
+    form <- stats::as.formula("y ~ 1")
+  } else if(ncol(data) == 2) {
+    names(data) <- c("y", "x")
+    form <- stats::as.formula("y ~ x")
   } else {
-    dfxm <- mlogit::mlogit.data(dfx, choice="y", shape = "wide", alt.levels=levels(y))
-    fit <- mlogit::mlogit(y ~ 0 | x, data = dfxm)
+    stop("fit_model expects two variables as input")
   }
-  fit
+
+  nnet::multinom(form, data=data, trace=FALSE, model=TRUE)
 }
 
 
@@ -175,38 +80,25 @@ fit_multinomial_model2 <- function(y, x=NULL) {
 #'
 #' The default function uses the MASS::polr function
 #'
-#' @param y the dependent variable
-#' @param x an optional predictor
+#' @param data a dataframe with the dependent variable and an optional covariate
 #'
 #' @return the model fit
 #' @export
-fit_ordinal_model <- function(y, x=NULL) {
+fit_ordinal_model <- function(data) {
 
-  dfx <- make_data(y, x)
-  form <- make_formula(y, x)
-  fit <- MASS::polr(form, data=dfx, Hess=TRUE)
-  fit
+  if (ncol(data) == 1) {
+    names(data) <- "y"
+    form <- stats::as.formula("y ~ 1")
+  } else if(ncol(data) == 2) {
+    names(data) <- c("y", "x")
+    form <- stats::as.formula("y ~ x")
+  } else {
+    stop("fit_model expects two variables as input")
+  }
+
+  MASS::polr(form, data=data, Hess=TRUE)
 }
 
-
-#' Fit an ordinal model
-#'
-#' This model is used for multiple choice questions with more than two ordered alternatives
-#'
-#' Alternative implementation that uses the ordinal::clm function
-#'
-#' @param y the dependent variable
-#' @param x an optional predictor
-#'
-#' @return the model fit
-#' @export
-fit_ordinal_model2 <- function(y, x=NULL) {
-
-  dfx <- make_data(y, x)
-  form <- make_formula(y, x)
-  fit <- ordinal::clm(form, data=dfx)
-  fit
-}
 
 
 #' Fit a mixed effects linear model with a logistic link function
@@ -215,19 +107,40 @@ fit_ordinal_model2 <- function(y, x=NULL) {
 #'
 #' The default function uses the lme4:glmer function
 #'
-#' @param y the dependent variable
-#' @param x an optional predictor
+#' @param data a dataframe with the dependent variable and an optional covariate
 #'
 #' @return the model fit
 #' @export
-fit_mixed_model <- function(y, x=NULL) {
+fit_mixed_model <- function(data) {
+
+  qq <- names(data)[1]
+
+  if (ncol(data) == 1) {
+    names(data) <- "y"
+    form <- stats::as.formula("y ~ alternative + (1|id)")
+  } else if(ncol(data) == 2) {
+    names(data) <- c("y", "x")
+    form <- stats::as.formula("y ~ alternative * x + (1|id)")
+  } else {
+    stop("fit_model expects two variables as input")
+  }
+
+  yy <- compute_checkbox_presence(data$y)
+
+  longdata <- data %>%
+    tibble::add_column(yy) %>%
+    tidyr::unpack(yy) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(id = 1:dplyr::n()) %>%
+    dplyr::select(-.data$y) %>%
+    tidyr::pivot_longer(cols=tidyselect::starts_with("I", ignore.case=FALSE), names_to = "alternative", values_to = "y") %>%
+    dplyr::arrange(.data$id)
 
 
-  dfx <- make_long_data(y, x)
-  form <- make_mixed_formula(y, x)
-  fit <- suppressWarnings(lme4::glmer(form, data=dfx, family=stats::binomial))
-  fit
+  suppressWarnings(lme4::glmer(form, data=longdata, family=stats::binomial))
 }
+
+
 
 
 
@@ -237,19 +150,41 @@ fit_mixed_model <- function(y, x=NULL) {
 #'
 #' Alternative implementation that uses the stats::glm function (and fits a fixed effects model instead of a mixed effects model)
 #'
-#' @param y the dependent variable
-#' @param x an optional predictor
+#' @param data a dataframe with the dependent variable and an optional covariate
 #'
 #' @return the model fit
 #' @export
-fit_fixed_model <- function(y, x=NULL) {
+fit_fixed_model <- function(data) {
 
-  dfx <- make_long_data(y, x)
-  form <- make_fixed_formula(y, x)
+  qq <- names(data)[1]
 
-  fit <- stats::glm(form, data=dfx, family=stats::binomial)
-  fit
+  if (ncol(data) == 1) {
+    names(data) <- "y"
+    form <- stats::as.formula("y ~ alternative")
+  } else if(ncol(data) == 2) {
+    names(data) <- c("y", "x")
+    form <- stats::as.formula("y ~ alternative * x")
+  } else {
+    stop("fit_model expects two variables as input")
+  }
+
+  yy <- compute_checkbox_presence(data$y)
+
+  longdata <- data %>%
+    tibble::add_column(yy) %>%
+    tidyr::unpack(yy) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(id = 1:dplyr::n()) %>%
+    dplyr::select(-.data$y) %>%
+    tidyr::pivot_longer(cols=tidyselect::starts_with("I", ignore.case=FALSE), names_to = "alternative", values_to = "y") %>%
+    dplyr::arrange(.data$id)
+
+  stats::glm(form, data=longdata, family=stats::binomial)
 }
+
+
+
+
 
 
 
