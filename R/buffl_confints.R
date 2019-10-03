bb0bc8d690972c40015c7a1a253d1dd54f2b0cf1
Michael Stevens <michael.andre.stevens@gmail.com>
2019-10-03 16:12:43 +0200

    added confints for one-predictor models

--- R/buffl_confints.R
+++ R/buffl_confints.R
@@ -7,11 +7,21 @@
 confint_linear_model <- function(fit) {
 
   stopifnot(class(fit)=="lm")
+  stopifnot(ncol(fit$model) %in% c(1, 2))
+  vars <- all.vars(stats::formula(fit))
 
-  stats::predict(fit, newdata=data.frame(1), se=TRUE, interval="confidence") %>%
-    purrr::pluck(1) %>%
-    data.frame() %>%
-    dplyr::rename(lower=.data$lwr, upper=.data$upr)
+  if (length(vars)==1) {
+    stats::predict(fit, data.frame(1), se=TRUE, interval="confidence") %>%
+      purrr::pluck(1) %>%
+      data.frame() %>%
+      dplyr::rename(lower=.data$lwr, upper=.data$upr)
+  } else if (length(vars)==2) {
+    effects::effect(vars[2], fit) %>%
+      data.frame() %>%
+      dplyr::select(-.data$se)
+  } else {
+    stop("confint_model expects a model with maximally one predictor")
+  }
 }
 
 
@@ -27,138 +37,107 @@ confint_logistic_model <- function(fit) {
   stopifnot(stats::family(fit)$family == "binomial")
   stopifnot(stats::family(fit)$link == "logit")
 
-  stats::predict(fit,  newdata=data.frame(1), type="link", se.fit=TRUE) %>%
-    data.frame() %>%
-    dplyr::mutate(lower=.data$fit-1.96*.data$se.fit) %>%
-    dplyr::mutate(upper=.data$fit+1.96*.data$se.fit) %>%
-    dplyr::select(-'se.fit', -'residual.scale') %>%
-    dplyr::mutate_all(stats::plogis)
-}
+  vars <- all.vars(stats::formula(fit))
 
-
-#' Confidence intervals around the intercept of a multinomial model
-#'
-#' @param fit the fitted model
-#'
-#' @return intercept estimate with confidence interval
-#' @export
-confint_multinomial_model <- function(fit) {
-
-  stopifnot(all(class(fit)==c("multinom", "nnet")))
-
-  # use the delta method
-  ncat <- nrow(stats::coef(fit))
-  parameterNames <- paste0("b", 1:ncat)
-
-  denominator <- paste0("exp(", parameterNames, ")", collapse="+")
-  denominator <- paste0("(1+", denominator, ")")
-
-  g <- vector("character", ncat+1)
-  g[1] <- paste0("1/", denominator)
-  for(i in 1:ncat) {
-    g[i+1] <- paste0("exp(", parameterNames[i], ")/", denominator)
+  if (length(vars)==1) {
+    out <- stats::predict(fit,  newdata=data.frame(1), type="link", se.fit=TRUE) %>%
+      data.frame() %>%
+      dplyr::mutate(lower=.data$fit-1.96*.data$se.fit) %>%
+      dplyr::mutate(upper=.data$fit+1.96*.data$se.fit) %>%
+      dplyr::select(-'se.fit', -'residual.scale') %>%
+      dplyr::mutate_all(stats::plogis)
+  } else if (length(vars)==2) {
+    out <- effects::effect(vars[2], fit) %>%
+      data.frame() %>%
+      dplyr::select(-.data$se)
+  } else {
+    stop("confint_model expects a model with maximally one predictor")
   }
 
-  out <- list()
-  for(i in 1:length(g)) {
-    out[[i]] <- car::deltaMethod(fit, g[i], parameterNames=parameterNames)
-  }
+  lvls <-  levels(fit$data[,1] %>% dplyr::pull())
 
-  out <- dplyr::bind_rows(out) %>%
-    dplyr::rename(fit=.data$Estimate) %>%
-    dplyr::select(-.data$SE) %>%
-    dplyr::rename(lower=.data$`2.5 %`) %>%
-    dplyr::rename(upper=.data$`97.5 %`)
+  out2 <- out %>%
+    dplyr::mutate(fit=1-.data$fit) %>%
+    dplyr::mutate(tmp=.data$lower) %>%
+    dplyr::mutate(upper=1-.data$lower) %>%
+    dplyr::mutate(lower=1-.data$tmp) %>%
+    dplyr::select(-.data$tmp) %>%
+    dplyr::mutate(alternative=lvls[1])
 
-  rownames(out) <- fit$lev
+  out <- out %>% dplyr::mutate(alternative=lvls[2])
+
+  out <- dplyr::bind_rows(out, out2) %>%
+    dplyr::select(.data$alternative, tidyselect::everything())
+
   out
 }
 
 
-#' Confidence intervals around the intercept of an ordinal model
+#' Confidence intervals around the intercept of a logistic fixed effects model
 #'
 #' @param fit the fitted model
 #'
 #' @return intercept estimate with confidence interval
 #' @export
-confint_ordinal_model <- function(fit) {
+confint_fixed_model <- function(fit) {
 
-  stopifnot(class(fit)=="polr")
+  stopifnot(all(class(fit)==c("glm", "lm")))
+  stopifnot(stats::family(fit)$family == "binomial")
+  stopifnot(stats::family(fit)$link == "logit")
 
-  # use the delta method
-  # polr intercepts are stored in zeta, not in beta
-  # we need a wrapper around the delta method
-  # that takes this into account
-  mydeltamethod <- function(fit, g.) {
-      cf <- fit$zeta
-      vcv <- stats::vcov(fit)
-      names(cf) <- paste("b", 1:length(cf), sep="")
-      rownames(vcv) <- colnames(vcv) <- names(cf)
-      car::deltaMethod(cf, g., vcov.=vcv)
+  vars <- all.vars(stats::formula(fit))
+
+  if (length(vars)==2) {
+    out <- effects::effect(vars[2], fit) %>%
+      data.frame() %>%
+      dplyr::select(-.data$se)
+  } else if (length(vars)==3) {
+    out <-  effects::effect(paste(vars[2], vars[3], sep=":"), fit) %>%
+      data.frame() %>%
+      dplyr::select(-.data$se)
+  } else {
+    stop("confint_model expects a model with maximally one predictor")
   }
+  out %>% dplyr::arrange(.data$alternative)
+}
 
-  # compute functions for cumulative, degressive and mass probabilities
-  ncat <- length(fit$zeta)
-  parameterNames <- paste0("b", 1:ncat)
 
-  g_cum <- vector("character", ncat)
-  for(i in 1:length(g_cum)) {
-    g_cum[i] <- paste0("exp(", parameterNames[i], ")/(1+exp(", parameterNames[i], "))")
+#' Confidence intervals around the intercept of a logistic mixed effects model
+#'
+#' @param fit the fitted model
+#'
+#' @return intercept estimate with confidence interval
+#' @export
+confint_mixed_model <- function(fit) {
+
+  stopifnot(class(fit)=="glmerMod")
+
+  vars <- all.vars(stats::formula(fit))
+
+    # work around bug
+  if(exists("longdata")) {
+    longdata5684354543450000 <- longdata
   }
+  longdata <<- fit@frame
 
-  g_revcum <- vector("character", ncat)
-  for(i in 1:length(g_revcum)) {
-    g_revcum[i] <- paste0("1-", g_cum[i])
+  if (length(vars)==3) {
+    out <- effects::effect(vars[2], fit) %>%
+      data.frame() %>%
+      dplyr::select(-.data$se)
+  } else if (length(vars)==4) {
+    out <- effects::effect(paste(vars[2], vars[3], sep=":"), fit) %>%
+      data.frame() %>%
+      dplyr::select(-.data$se)
+  } else {
+    if(exists("longdata5684354543450000")) {
+      longdata <- longdata5684354543450000
+    }
+    stop("confint_model expects a model with maximally one predictor")
   }
-
-  g_prob <- vector("character", ncat+1)
-  g_prob[1] <- g_cum[1]
-  for(i in 2:(length(g_prob)-1)) {
-    g_prob[i] <- paste0(g_cum[i], " - ", g_cum[i-1])
+  if(exists("longdata5684354543450000")) {
+    longdata <- longdata5684354543450000
   }
-  g_prob[length(g_cum)+1] <- paste0(1, " - ", g_cum[length(g_cum)])
-
-  # apply the delta method on the functions
-  out_cum <- list()
-  for(i in 1:length(g_cum)) {
-    out_cum[[i]] <- mydeltamethod(fit, g_cum[i])
-  }
-
-  out_revcum <- list()
-  for(i in 1:length(g_revcum)) {
-    out_revcum[[i]] <- mydeltamethod(fit, g_revcum[i])
-  }
-
-  out_prob <- list()
-  for(i in 1:length(g_prob)) {
-    out_prob[[i]] <- mydeltamethod(fit, g_prob[i])
-  }
-
-  out_cum <- dplyr::bind_rows(out_cum) %>%
-    dplyr::rename(fit=.data$Estimate) %>%
-    dplyr::select(-.data$SE) %>%
-    dplyr::rename(lower=.data$`2.5 %`) %>%
-    dplyr::rename(upper=.data$`97.5 %`) %>%
-    tibble::add_row(fit=1, lower=1, upper=1)
-
-  out_revcum <- dplyr::bind_rows(out_revcum) %>%
-    dplyr::rename(fit=.data$Estimate) %>%
-    dplyr::select(-.data$SE) %>%
-    dplyr::rename(lower=.data$`2.5 %`) %>%
-    dplyr::rename(upper=.data$`97.5 %`) %>%
-    tibble::add_row(fit=1, lower=1, upper=1, .before=1)
-
-  out_prob <- dplyr::bind_rows(out_prob) %>%
-    dplyr::rename(fit=.data$Estimate) %>%
-    dplyr::select(-.data$SE) %>%
-    dplyr::rename(lower=.data$`2.5 %`) %>%
-    dplyr::rename(upper=.data$`97.5 %`)
-
-  rownames(out_cum) <- fit$lev
-  rownames(out_revcum) <- fit$lev
-  rownames(out_prob) <- fit$lev
-
-  list("probability"=out_prob, "cumulative probability"=out_cum, "reverse cumulative probability"=out_revcum)
+  out %>% dplyr::arrange(.data$alternative)
 }
 
 
@@ -168,7 +147,7 @@ confint_ordinal_model <- function(fit) {
 #'
 #' @return category estimates with confidence interval
 #' @export
-confint_mixed_model <- function(fit) {
+confint_mixed_model_bolker <- function(fit) {
 
   stopifnot(class(fit)=="glmerMod")
 
@@ -199,36 +178,210 @@ confint_mixed_model <- function(fit) {
   newdata$lower_rf <- stats::plogis(newdata$lower_rf)
   newdata$upper_rf <- stats::plogis(newdata$upper_rf)
 
+  nd <- names(newdata)
   newdata %>%
-    dplyr::select(-.data$y, -.data$id) %>%
+    dplyr::select(-tidyselect::matches(nd[1]), -.data$id) %>%
     data.frame(row.names=NULL) %>%
     tibble::column_to_rownames(var="alternative")
 }
 
 
-#' Confidence intervals around the intercept of a logistic fixed effects model
+#' Compute effects for a multinomial/ordinal model with one predictor
+#'
+#' @param fit the fitted model
+#'
+#' @return effect estimates with confidence interval
+#' @keywords internal
+confint_multinomial_ordinal_model <- function(fit) {
+
+  term <- stringr::str_split(fit$terms, "~")[[3]]
+  fx <- effects::effect(term, fit)
+
+  addx <- function(x) x %>% dplyr::mutate(X=paste0("X", 1:dplyr::n()))
+  addy <- function(x) x %>% dplyr::mutate(Y=paste0("Y", 1:dplyr::n()))
+  set_names <- function(x) {
+    stats::setNames(x, paste0("Y", 1:ncol(x)))
+  }
+
+  fxx <- fx$x %>% addx()
+  fyy <- data.frame(alternative=fx$y) %>% addy()
+
+  longform <- function(x) {
+    x %>%
+      data.frame() %>%
+      set_names() %>%
+      addx() %>%
+      tidyr::gather("Y", "value", -.data$X)
+  }
+
+  fxf <- fx$prob %>% longform() %>% dplyr::rename(fit=.data$value)
+  fxl <- fx$lower.prob %>% longform() %>% dplyr::rename(lower=.data$value)
+  fxu <- fx$upper.prob %>% longform() %>% dplyr::rename(upper=.data$value)
+
+  fxf %>%
+    dplyr::inner_join(fxl, by = c("X", "Y")) %>%
+    dplyr::inner_join(fxu, by = c("X", "Y")) %>%
+    dplyr::inner_join(fxx, by = "X") %>%
+    dplyr::inner_join(fyy, by = "Y") %>%
+    dplyr::arrange(.data$Y, .data$X) %>%
+    dplyr::select(.data$alternative, tidyselect::matches(term),
+                  .data$fit, .data$lower, .data$upper) %>%
+    dplyr::mutate(alternative=forcats::fct_inorder(.data$alternative))
+}
+
+#' Confidence intervals around the intercept of a multinomial model
 #'
 #' @param fit the fitted model
 #'
 #' @return intercept estimate with confidence interval
 #' @export
-confint_fixed_model <- function(fit) {
+confint_multinomial_model <- function(fit) {
 
-  stopifnot(all(class(fit)==c("glm", "lm")))
-  stopifnot(stats::family(fit)$family == "binomial")
-  stopifnot(stats::family(fit)$link == "logit")
+  stopifnot(all(class(fit)==c("multinom", "nnet")))
+  vars <- all.vars(stats::formula(fit))
 
-  newdata <- fit$data %>% dplyr::filter(.data$id==1)
+  if (length(vars)==1) {
 
-  out <- stats::predict(fit, newdata, type="link", se.fit=TRUE) %>%
-    data.frame() %>%
-    dplyr::mutate(lower=.data$fit-1.96*.data$se.fit) %>%
-    dplyr::mutate(upper=.data$fit+1.96*.data$se.fit) %>%
-    dplyr::select(-'se.fit', -'residual.scale') %>%
-    dplyr::mutate_all(stats::plogis)
+    # use the delta method
+    ncat <- nrow(stats::coef(fit))
+    parameterNames <- paste0("b", 1:ncat)
 
-  rownames(out) <- newdata$alternative
-  out
+    denominator <- paste0("exp(", parameterNames, ")", collapse="+")
+    denominator <- paste0("(1+", denominator, ")")
+
+    g <- vector("character", ncat+1)
+    g[1] <- paste0("1/", denominator)
+    for(i in 1:ncat) {
+      g[i+1] <- paste0("exp(", parameterNames[i], ")/", denominator)
+    }
+
+    out <- list()
+    for(i in 1:length(g)) {
+      out[[i]] <- car::deltaMethod(fit, g[i], parameterNames=parameterNames)
+    }
+
+    out <- dplyr::bind_rows(out) %>%
+      dplyr::rename(fit=.data$Estimate) %>%
+      dplyr::select(-.data$SE) %>%
+      dplyr::rename(lower=.data$`2.5 %`) %>%
+      dplyr::rename(upper=.data$`97.5 %`)
+
+    alternatives <- fit$lev
+    out %>%
+      dplyr::mutate(alternative=alternatives) %>%
+      dplyr::select(.data$alternative, tidyselect::everything())
+
+  } else if (length(vars)==2) {
+    confint_multinomial_ordinal_model(fit)
+  } else {
+    stop("confint_model expects a model with maximally one predictor")
+  }
+}
+
+
+#' Confidence intervals around the intercept of an ordinal model
+#'
+#' @param fit the fitted model
+#'
+#' @return intercept estimate with confidence interval
+#' @export
+confint_ordinal_model <- function(fit) {
+
+  stopifnot(class(fit)=="polr")
+
+  vars <- all.vars(stats::formula(fit))
+
+  if (length(vars)==1) {
+
+    ncat <- length(fit$zeta)
+    beta_names <- NULL
+    if(length(stats::coef(fit))>0) {
+      beta_names <- paste0("beta", seq_along(stats::coef(fit)))
+    }
+    zeta_names <- paste0("zeta", seq_along(fit$zeta))
+    parameterNames <- c(beta_names, zeta_names)
+
+    # use the delta method
+    # polr intercepts are stored in zeta, not in beta
+    # we need a wrapper around the delta method
+    # that takes this into account
+    mydeltamethod <- function(fit, g.) {
+        cf <- c(stats::coef(fit), fit$zeta)
+        vcv <- stats::vcov(fit)
+        rownames(vcv) <- colnames(vcv) <- names(cf) <- parameterNames
+        car::deltaMethod(cf, g., vcov.=vcv)
+    }
+
+    reshape_prediction <- function(x) {
+      dplyr::bind_rows(x) %>%
+        dplyr::rename(fit=.data$Estimate) %>%
+        dplyr::select(-.data$SE) %>%
+        dplyr::rename(lower=.data$`2.5 %`) %>%
+        dplyr::rename(upper=.data$`97.5 %`)
+    }
+
+    add_alternative <- function(x, prob="probability") {
+      lvl <- fit$lev
+      x %>%
+        dplyr::mutate(alternative=lvl) %>%
+        dplyr::mutate(probability=prob) %>%
+        dplyr::select(.data$probability, .data$alternative, tidyselect::everything())
+
+    }
+
+    # compute functions for cumulative, reverse cumulative and marginal probabilities
+    # apply the delta method on the functions
+
+    # cumulative probability
+    g_cum <- vector("character", ncat)
+    out_cum <- list()
+    for(i in 1:length(g_cum)) {
+      g_cum[i] <- paste0("exp(", parameterNames[i], ")/(1+exp(", parameterNames[i], "))")
+      out_cum[[i]] <- mydeltamethod(fit, g_cum[i])
+    }
+    out_cum <- reshape_prediction(out_cum) %>%
+      tibble::add_row(fit=1, lower=1, upper=1) %>%
+      add_alternative("cumulative probability")
+
+    # reverse cumulatve probability
+    g_revcum <- vector("character", ncat)
+    out_revcum <- list()
+    for(i in 1:length(g_revcum)) {
+      g_revcum[i] <- paste0("1-", g_cum[i])
+      out_revcum[[i]] <- mydeltamethod(fit, g_revcum[i])
+    }
+    out_revcum <- reshape_prediction(out_revcum) %>%
+      tibble::add_row(fit=1, lower=1, upper=1, .before=1) %>%
+      add_alternative("reverse cumulatve probability")
+
+    # marginal probability
+    g_prob <- vector("character", ncat+1)
+    g_prob[1] <- g_cum[1]
+    out_prob <- list()
+    for(i in 2:(length(g_prob)-1)) {
+      g_prob[i] <- paste0(g_cum[i], " - ", g_cum[i-1])
+    }
+    g_prob[length(g_cum)+1] <- paste0(1, " - ", g_cum[length(g_cum)])
+
+    for(i in 1:length(g_prob)) {
+      out_prob[[i]] <- mydeltamethod(fit, g_prob[i])
+    }
+
+    out_prob <- reshape_prediction(out_prob) %>%
+      add_alternative("probability mass")
+
+    print(g_prob)
+    #print(g_cum)
+    #print(g_revcum)
+    dplyr::bind_rows(out_prob, out_cum, out_revcum)
+
+  } else if (length(vars)==2) {
+
+    confint_multinomial_ordinal_model(fit)
+
+  } else {
+    stop("confint_model expects a model with maximally one predictor")
+  }
 }
 
 
--- R/buffl_fitters.R
+++ R/buffl_fitters.R
@@ -8,19 +8,16 @@
 #' @export
 fit_linear_model <- function(data) {
 
-  if (ncol(data) == 1) {
-    names(data) <- "y"
-    form <- stats::as.formula("y ~ 1")
-  } else if(ncol(data) == 2) {
-    names(data) <- c("y", "x")
-    form <- stats::as.formula("y ~ x")
+  nd <- names(data)
+  if (length(nd) == 1) {
+    form <- stats::as.formula(paste(nd, "~ 1"))
+  } else if(length(nd) == 2) {
+    form <- stats::as.formula(paste(nd[1], "~", nd[2]))
   } else {
-    stop("fit_model expects two variables as input")
+    stop("fit_model expects maximally two variables as input")
   }
-
   stats::lm(form, data=data)
 }
-
 
 
 #' Fit a linear model with a logistic link function
@@ -33,115 +30,47 @@ fit_linear_model <- function(data) {
 #' @export
 fit_logistic_model <- function(data) {
 
-  if (ncol(data) == 1) {
-    names(data) <- "y"
-    form <- stats::as.formula("y ~ 1")
-  } else if(ncol(data) == 2) {
-    names(data) <- c("y", "x")
-    form <- stats::as.formula("y ~ x")
+  nd <- names(data)
+  if (length(nd) == 1) {
+    form <- stats::as.formula(paste(nd, "~ 1"))
+  } else if(length(nd) == 2) {
+    form <- stats::as.formula(paste(nd[1], "~", nd[2]))
   } else {
-    stop("fit_model expects two variables as input")
+    stop("fit_model expects maximally two variables as input")
   }
-
   stats::glm(form, data=data, family=stats::binomial)
 }
 
 
-
-#' Fit a multinomial model
-#'
-#' This model is used for multiple choice questions with more than two unordered alternatives
-#'
-#' The default function uses the nnet::multinom function
-#'
-#' @param data a dataframe with the dependent variable and an optional covariate
-#'
-#' @return the model fit
-#' @export
-fit_multinomial_model <- function(data) {
-
-  if (ncol(data) == 1) {
-    names(data) <- "y"
-    form <- stats::as.formula("y ~ 1")
-  } else if(ncol(data) == 2) {
-    names(data) <- c("y", "x")
-    form <- stats::as.formula("y ~ x")
-  } else {
-    stop("fit_model expects two variables as input")
-  }
-
-  nnet::multinom(form, data=data, trace=FALSE, model=TRUE)
-}
-
-
-#' Fit an ordinal model
-#'
-#' This model is used for multiple choice questions with more than two ordered alternatives
-#'
-#' The default function uses the MASS::polr function
-#'
-#' @param data a dataframe with the dependent variable and an optional covariate
-#'
-#' @return the model fit
-#' @export
-fit_ordinal_model <- function(data) {
-
-  if (ncol(data) == 1) {
-    names(data) <- "y"
-    form <- stats::as.formula("y ~ 1")
-  } else if(ncol(data) == 2) {
-    names(data) <- c("y", "x")
-    form <- stats::as.formula("y ~ x")
-  } else {
-    stop("fit_model expects two variables as input")
-  }
-
-  MASS::polr(form, data=data, Hess=TRUE)
-}
-
-
-
-#' Fit a mixed effects linear model with a logistic link function
+#' Prepare data for a fixed or mixed linear model fit
 #'
 #' This model is used for checkbox questions (multiple choice where more than one alternative can be selected)
 #'
-#' The default function uses the lme4:glmer function
-#'
 #' @param data a dataframe with the dependent variable and an optional covariate
 #'
-#' @return the model fit
-#' @export
-fit_mixed_model <- function(data) {
+#' @return the data in long format
+#' @keywords internal
+make_long_data <- function(data) {
 
-  qq <- names(data)[1]
 
-  if (ncol(data) == 1) {
-    names(data) <- "y"
-    form <- stats::as.formula("y ~ alternative + (1|id)")
-  } else if(ncol(data) == 2) {
-    names(data) <- c("y", "x")
-    form <- stats::as.formula("y ~ alternative * x + (1|id)")
-  } else {
-    stop("fit_model expects two variables as input")
-  }
+  y <- data[,1] %>% dplyr::pull()
+  yy <- compute_checkbox_presence(y)
+  ymap <- data.frame(alternative=attr(y, "levels"), alternative.idx=names(yy), stringsAsFactors=FALSE) %>%
+    dplyr::mutate(alternative=forcats::fct_inorder(.data$alternative))
 
-  yy <- compute_checkbox_presence(data$y)
+  nd <- names(data)
 
-  longdata <- data %>%
+    longdata <- data %>%
     tibble::add_column(yy) %>%
     tidyr::unpack(yy) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(id = 1:dplyr::n()) %>%
-    dplyr::select(-.data$y) %>%
-    tidyr::pivot_longer(cols=tidyselect::starts_with("I", ignore.case=FALSE), names_to = "alternative", values_to = "y") %>%
+    dplyr::select(-tidyselect::matches(nd[1])) %>%
+    tidyr::pivot_longer(cols=tidyselect::starts_with("I", ignore.case=FALSE), names_to = "alternative.idx", values_to = nd[1]) %>%
     dplyr::arrange(.data$id)
 
-
-  suppressWarnings(lme4::glmer(form, data=longdata, family=stats::binomial))
+  longdata %>% dplyr::left_join(ymap, by="alternative.idx")
 }
-
-
-
 
 
 #' Fit a fixed effects linear model with a logistic link function
@@ -156,36 +85,96 @@ fit_mixed_model <- function(data) {
 #' @export
 fit_fixed_model <- function(data) {
 
-  qq <- names(data)[1]
-
-  if (ncol(data) == 1) {
-    names(data) <- "y"
-    form <- stats::as.formula("y ~ alternative")
-  } else if(ncol(data) == 2) {
-    names(data) <- c("y", "x")
-    form <- stats::as.formula("y ~ alternative * x")
+  nd <- names(data)
+  if (length(nd) == 1) {
+    form <- stats::as.formula(paste(nd, "~ alternative"))
+  } else if(length(nd) == 2) {
+    form <- stats::as.formula(paste(nd[1], "~ alternative *", nd[2]))
   } else {
-    stop("fit_model expects two variables as input")
+    stop("fit_model expects maximally two variables as input")
   }
 
-  yy <- compute_checkbox_presence(data$y)
-
-  longdata <- data %>%
-    tibble::add_column(yy) %>%
-    tidyr::unpack(yy) %>%
-    dplyr::ungroup() %>%
-    dplyr::mutate(id = 1:dplyr::n()) %>%
-    dplyr::select(-.data$y) %>%
-    tidyr::pivot_longer(cols=tidyselect::starts_with("I", ignore.case=FALSE), names_to = "alternative", values_to = "y") %>%
-    dplyr::arrange(.data$id)
+  longdata <- make_long_data(data)
 
   stats::glm(form, data=longdata, family=stats::binomial)
 }
 
 
+#' Fit a mixed effects linear model with a logistic link function
+#'
+#' This model is used for checkbox questions (multiple choice where more than one alternative can be selected)
+#'
+#' The default function uses the lme4:glmer function
+#'
+#' @param data a dataframe with the dependent variable and an optional covariate
+#'
+#' @return the model fit
+#' @export
+fit_mixed_model <- function(data) {
+
+  nd <- names(data)
+  if (length(nd) == 1) {
+    form <- stats::as.formula(paste(nd, "~ alternative + (1|id)"))
+  } else if(length(nd) == 2) {
+    form <- stats::as.formula(paste(nd[1], "~ alternative *", nd[2], "+ (1|id)"))
+  } else {
+    stop("fit_model expects maximally two variables as input")
+  }
+
+  longdata <- make_long_data(data)
+
+  suppressWarnings(lme4::glmer(form, data=longdata, family=stats::binomial))
+}
+
+
+#' Fit a multinomial model
+#'
+#' This model is used for multiple choice questions with more than two unordered alternatives
+#'
+#' The default function uses the nnet::multinom function
+#'
+#' @param data a dataframe with the dependent variable and an optional covariate
+#'
+#' @return the model fit
+#' @export
+fit_multinomial_model <- function(data) {
+
+  nd <- names(data)
+  if (length(nd) == 1) {
+    form <- stats::as.formula(paste(nd, "~ 1"))
+  } else if(length(nd) == 2) {
+    form <- stats::as.formula(paste(nd[1], "~", nd[2]))
+  } else {
+    stop("fit_model expects maximally two variables as input")
+  }
+
+  nnet::multinom(form, data=data, trace=FALSE, model=TRUE)
+}
 
 
 
+#' Fit an ordinal model
+#'
+#' This model is used for multiple choice questions with more than two ordered alternatives
+#'
+#' The default function uses the MASS::polr function
+#'
+#' @param data a dataframe with the dependent variable and an optional covariate
+#'
+#' @return the model fit
+#' @export
+fit_ordinal_model <- function(data) {
 
+  nd <- names(data)
+  if (length(nd) == 1) {
+    form <- stats::as.formula(paste(nd, "~ 1"))
+  } else if(length(nd) == 2) {
+    form <- stats::as.formula(paste(nd[1], "~", nd[2]))
+  } else {
+    stop("fit_model expects maximally two variables as input")
+  }
+
+  MASS::polr(form, data=data, Hess=TRUE)
+}
 
 
